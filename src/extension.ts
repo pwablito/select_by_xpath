7d21d96c913c999d4cac6e932e342fd2bbb4e33f
pwablito <pwablito@gmail.com>
2021-06-22 10:45:18 -0700

    Register two actions

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "select-by-xpath" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('select-by-xpath.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Select By XPath!');
-	});
-
-	context.subscriptions.push(disposable);
+function xpath_to_css_selector(xpath: string) {
+	//TODO implement conversion/parsing
+	return xpath;
 }
 
-// this method is called when your extension is deactivated
+function css_selector_to_js_selector(css_selector: string) {
+	return `document.querySelector("${css_selector}")`;
+}
+
+
+export function activate(context: vscode.ExtensionContext) {
+	
+	console.log('Activated select-by-xpath');
+
+	context.subscriptions.push(vscode.commands.registerCommand('select-by-xpath.convert_text', async () => {
+		let xpath: string | undefined = await vscode.window.showInputBox({
+			ignoreFocusOut: true,
+			placeHolder: 'Example: /html/body/button',
+			prompt: 'Input an XPath',
+		});
+		if (!xpath) {
+			vscode.window.showInformationMessage('Empty input XPath');
+			return;
+		}
+		let js_selector = css_selector_to_js_selector(xpath_to_css_selector(xpath));
+		await vscode.window.showInputBox({
+			ignoreFocusOut: true,
+			value: js_selector,
+			prompt: 'Raw JS Selector',
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('select-by-xpath.convert_clipboard', async () => {
+		let xpath: string = await vscode.env.clipboard.readText();
+		if (!xpath) {
+			vscode.window.showInformationMessage('Empty input XPath');
+			return;
+		}
+		let js_selector = css_selector_to_js_selector(xpath_to_css_selector(xpath));
+		await vscode.env.clipboard.writeText(js_selector);
+		vscode.window.showInformationMessage('Clipboard XPATH converted to JS selector');
+	}));
+}
+
 export function deactivate() {}
